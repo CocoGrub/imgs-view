b767c800197acdd76315a406da777ca3aaac5965
CocoGrub <fedsavin@gmail.com>
2020-10-03 14:39:41 +0300

    dist

---
+++ build/precache-manifest.72e72ace15d8ff85bcc5a90f901a6e07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be7ad6a26c063d376202896b502db1cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afafcbd0244bd2c5991d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "473f917f2b7eb348c586",
+    "url": "/static/js/2.cbc76a3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cbc76a3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afafcbd0244bd2c5991d",
+    "url": "/static/js/main.c6253f7a.chunk.js"
+  },
+  {
+    "revision": "6d6b44f472b88cd48717",
+    "url": "/static/js/runtime-main.ad8b68f7.js"
+  }
+]);
