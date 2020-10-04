45e017ce8477e6720da79f84611a2e66583cc521
CocoGrub <fedsavin@gmail.com>
2020-10-04 23:35:54 +0300

    time

--- src/components/card.js
+++ src/components/card.js
@@ -2,28 +2,32 @@ import React, { useState, useEffect } from "react";
 import "./card.css";
 import { fetchUserName,fetchPhoto } from "../api/api";
 const Card = ({ photo }) => {
-  const { id, title, count_faves, owner,dateupload} = photo;
+  const { id, title, count_faves, owner} = photo;
   const [profile, setProfile] = useState("");
   const [galleryPhoto,setGalleryPhoto]=useState('')
   useEffect(() => {
     fetchUserName(owner).then((res) => {
       setProfile(res.profile);
     });
-  }, []);
+  }, [owner]);
   useEffect(()=>{
     fetchPhoto(id).then((res)=>{
       setGalleryPhoto(res.sizes.size[4].source)
     })
-  },[])
-  const formattedDate=new Date(dateupload)
-  const {country,first_name}=profile
+  },[id])
+  const formattedDate=function (min, max) {
+      let randomNum = Math.random() * (max - min) + min;
+      return Math.floor(randomNum);
+  }
+
+  const {country}=profile
   const formattedCountry= (country + "").slice(0, 15);
-  console.log(dateupload);
+
 
   return( <div className="card">
             <div className={"head"}>
             <div className={"person-info"}>
-              <img src={`http://farm9.staticflickr.com/8573/buddyicons/${owner}.jpg`}/>
+              <img src={`http://farm9.staticflickr.com/8573/buddyicons/${owner}.jpg`} alt={'buddy'}/>
               <div className={"name-place"}>
                 <b>{profile.first_name}</b>
                 <i>{formattedCountry}</i>
@@ -31,15 +35,17 @@ const Card = ({ photo }) => {
             </div>
 
               <div>
-                2h
+                  {formattedDate(1,24)}h
               </div>
             </div>
           <div className={'main-image'}>
-            <img src={galleryPhoto}/>
+            <img src={galleryPhoto} alt={'main'}/>
           </div>
 
           <div className={"bottom"}>
-          <span>{count_faves}</span>
+          <span ><i className="far fa-heart" onClick={()=>{
+              alert(id)
+          }}> &nbsp;</i>{count_faves}</span>
             <br/>
           <span>{title}</span>
           </div>
